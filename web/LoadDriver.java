a7fe9b04347f2a4f470b4e6d540c5315e49487a4
cRaZy_LioN <cRaZy_LioN@DESKTOP-CHNN2TV>
2017-09-20 20:50:29 +0530

    First commit

---
+++ web/LoadDriver.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cRaZy_LioN
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Notice, do not import com.mysql.jdbc.*
+// or you will have problems!
+
+public class LoadDriver {
+    public static void main(String[] args) {
+Connection conn = null;
+try {
+    conn =
+       DriverManager.getConnection("jdbc:mysql://localhost/oams?" +
+                                   "user=root&password=root");
+
+    // Do something with the Connection
+
+} catch (SQLException ex) {
+    // handle any errors
+    System.out.println("SQLException: " + ex.getMessage());
+    System.out.println("SQLState: " + ex.getSQLState());
+    System.out.println("VendorError: " + ex.getErrorCode());
+}
+    }
+}
